f5348f62b7617ad923ac6ace54567373e2039355
tquandoo_24 <quantrungdo2401@gmail.com>
2024-05-05 11:28:10 +0700

    connect database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+mongoose
+    .connect(process.env.mongodbURI)
+    .then(() => {
+        console.log('MongoDB is conected')
+    }).catch((error) => {
+        console.log(error);
+    })
 const app = express()
 
 app.listen(3000, () => {
