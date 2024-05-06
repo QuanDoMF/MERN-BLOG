924b43bd106de92567a6a54916db8b2c278deaba
tquandoo_24 <quantrungdo2401@gmail.com>
2024-05-06 22:04:33 +0700

    create backend and run server

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import useRouter from '~/routes/userRoute'
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+const app = express()
+
+const hostname = 'localhost'
+const port = 3000
+
+app.listen(port, hostname, () => {
+  console.log(`Hello Trung Quan , Server running at http://${hostname}:${port}`)
+})
+app.get("/", (req, res) => {
+  res.json({ message: 'API is working' })
+})
+app.use('/api/user', useRouter)
