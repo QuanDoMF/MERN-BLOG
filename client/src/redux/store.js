ae7b873b4ee37854d7f5752cab9bac74fa3902fd
tquandoo_24 <quantrungdo2401@gmail.com>
2024-05-15 11:45:07 +0700

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './userSlice/userSlice'
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },        // các reducer
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { version } from 'react'
+import persistStore from 'redux-persist/es/persistStore'
+
+// tạo các reducer thành 1 rootReducer
+const rootReducer = combineReducers({
+    user: userReducer
 })
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+// tạo ra reducer có khả năng persist(lưu trữ)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = configureStore({
+    reducer: persistedReducer,
+    // được tùy chỉnh để bỏ qua kiểm tra tính serializable, vì redux-persist có thể chứa các giá trị không thể tuần tự hóa.
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }
+        )
+})
+
+// persistStore được sử dụng để tạo ra một persistor, quản lý quá trình lưu trữ và khôi phục trạng thái.
+export const persistor = persistStore(store);
--- client/src/redux/userSlice/userSlice.js
+++ client/src/redux/userSlice/userSlice.js
@@ -5,7 +5,6 @@ const initialState = {
     error: null,
     loading: false
 }
-
 const userSlice = createSlice({
     name: 'user',
     initialState,
